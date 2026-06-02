a510bb39939d50aae047bee4748522ac665dfc77
leere2424 <leere2424@khu.ac.kr>
2026-06-02 14:15:34 +0900

    update: settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,10 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-kf6c#@&3xkbo%g2roja8_g=u)rxp@k!9!=8xi-$us&cbc!2lzh')
 
